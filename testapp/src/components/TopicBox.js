6f6af5273cbdda35c5506e2fb68cd94aea4f133c
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-21 19:14:27 +0000

    creating dynamic components

--- testapp/src/components/TopicBox.js
+++ testapp/src/components/TopicBox.js
@@ -1,9 +1,11 @@
 import './topicbox.css';
 export default function TopicBox(){
+    const topic ="Hello, ";
+    const name ="I'm Ravisara Jayasundara"
     return(
         <div>
             <div className="topicBox">
-                <span className="text">Hello, I'm Ravisara Jayasundara</span>
+                <span className="text">{topic},{name}</span>
              </div>
         </div>
     )
