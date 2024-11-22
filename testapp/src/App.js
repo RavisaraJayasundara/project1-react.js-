2b5beb494b55700a86707b9c0bf2a033a4fb0e54
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-22 15:27:08 +0530

    Using useState

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -4,13 +4,17 @@ import Home from './pages/Home';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Profile from './pages/Profile';
 import About from './pages/About';
+import Firstpage from './pages/Firstpage';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">     
+    <div className="App"> 
+    
       <BrowserRouter>
           <Routes>
+          <Route path="/" element={<Firstpage/>}>   
+          </Route>
             <Route path="/Home" element={<Home/>}></Route>
             <Route path="/Profile" element={<Profile/>}></Route>
             <Route path="/About" element={<About/>}></Route>
---
+++ testapp/src/pages/Firstpage.js
@@ -0,0 +1,23 @@
+import '../components/firstpage.css' ;
+import {useState} from 'react';
+
+export default function Firstpage(){
+    const[count,setCount]=useState(0);
+    //const count =0;
+    const increment=()=>{
+        //count +=1;
+        setCount(count+1)
+    };
+    const decrement=() =>{
+        setCount (count-1);
+    }
+    return(
+        <div>
+            <h1>Welcome</h1>
+            <span className="title">My Counter</span>
+             <p className="subTitle">The count is {count}</p>
+             <button onClick = {decrement} className="button">-</button>
+             <button onClick = {increment} className="button">+</button>
+        </div>
+    )
+}
